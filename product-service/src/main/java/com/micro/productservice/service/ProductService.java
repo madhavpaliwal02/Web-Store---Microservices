71170b351044e1865b8eaf3b1723e0fbc9b1cb84
Madhav Paliwal <madhavpaliwal02@gmail.com>
2023-02-18 00:30:09 +0530

    Product Service moduled completed

--- product-service/src/main/java/com/micro/productservice/service/ProductService.java
+++ product-service/src/main/java/com/micro/productservice/service/ProductService.java
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductService {
 
-    // @Autowired
     private final ProductRepo productRepo;
 
     public void createProduct(ProductRequest productRequest) {
