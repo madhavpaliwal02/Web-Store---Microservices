1bebc3418be292079bff2a3a65f3931b616967e2
Madhav Paliwal <madhavpaliwal02@gmail.com>
2023-02-22 19:34:53 +0530

    Created new branch task1

--- web-store-micro/order-service/src/main/java/com/micro/orderservice/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.micro.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.micro.orderservice.model.Order;
-
-public interface OrderRepo extends JpaRepository<Order, Long> {
-
-}
