f68114e4ad3026f8c464b5ba49aeb301320ef309
Madhav Paliwal <madhavpaliwal02@gmail.com>
2023-04-03 11:41:27 +0530

    Minor modifications in product-service

--- web-store-micro/product-service/src/main/java/com/micro/productservice/service/ProductService.java
+++ web-store-micro/product-service/src/main/java/com/micro/productservice/service/ProductService.java
@@ -2,7 +2,6 @@ package com.micro.productservice.service;
 
 import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.micro.productservice.dto.ProductRequest;
